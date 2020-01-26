fe27ce54bc288a69581b4d0b2d1dd10ea927aa59
arifkoroglu <arifkoroglu19@gmail.com>
2020-01-27 00:29:43 +0300

    partials and layouts file

---
+++ resources/views/partials/theme/head.blade.php
@@ -0,0 +1,13 @@
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <title>SoftLab</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    
+  
+    <!--[if lt IE 9]>
+		<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="catalog/view/theme/fastor/js/respond.min.js"></script>
+	<![endif]-->
+
